985f51d8c1b8f7ea80ea2e8b40e3cf946990785d
CarlEverest78 <thepythoneer@gmail.com>
2022-05-30 16:51:32 +0200

    final commit added seoarate character js file and created folder structure

---
+++ character.js
@@ -0,0 +1,23 @@
+import {getDiceRollArray} from "./utils.js"
+
+
+function Character(data) {
+    Object.assign(this, data)
+    const { elementId, name, avatar, health, diceCount} = this;
+
+    this.getDiceHtml = () => getDiceRollArray(diceCount).map(dice => `<div class="dice">${dice}</div>`).join("")
+    
+    
+    this.getCharacterHtml = function() {
+        return  `<div class="character-card">
+                    <h4 class="name"> ${name} </h4>
+                    <img class="avatar" src="${avatar}" />
+                    <div class="health">health: <b> ${health} </b></div>
+                    <div class="dice-container">
+                        ${this.getDiceHtml()}
+                    </div>
+                </div>`
+    }
+}
+
+export default Character
--- index.js
+++ index.js
@@ -1,34 +1,15 @@
 import {characterData} from "./data.js"
-import {getDiceRollArray} from "./utils.js"
+import Character from "./character.js"
 
 
-function Character(data) {
-    Object.assign(this, data)
-    const { elementId, name, avatar, health, diceCount} = this;
-
-    this.getDiceHtml = () => getDiceRollArray(diceCount).map(dice => `<div class="dice">${dice}</div>`).join("")
-    
-    
-    this.getCharacterHtml = function() {
-        return  `<div class="character-card">
-                    <h4 class="name"> ${name} </h4>
-                    <img class="avatar" src="${avatar}" />
-                    <div class="health">health: <b> ${health} </b></div>
-                    <div class="dice-container">
-                        ${this.getDiceHtml()}
-                    </div>
-                </div>`
-    }
-}
 
 function render(data) {
-    document.getElementById(wizard.elementId).innerHTML = wizard.getCharacterHtml();
-    document.getElementById(orc.elementId).innerHTML = orc.getCharacterHtml();
+    document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
+    document.getElementById("monster").innerHTML = orc.getCharacterHtml();
 }
 
 /*Does something here need to change?*/
-const {hero,monster} = characterData
-const wizard = new Character(hero)
-const orc = new Character(monster)
+const wizard = new Character(characterData.hero)
+const orc = new Character(characterData.monster)
 render()
 document.getElementById("attack-button").addEventListener("click", () => render())
