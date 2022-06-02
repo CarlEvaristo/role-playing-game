fd18ffbec425b3fac28e73951203e12b9191b60e
CarlEverest78 <thepythoneer@gmail.com>
2022-06-02 10:39:03 +0200

    final commit

--- character.js
+++ character.js
@@ -1,23 +1,23 @@
 import {getDiceRollArray, getDicePlaceholderHtml, getPercentage } from "./utils.js"
 
+class Character {
+    constructor(data) {
+        Object.assign(this, data)
+        this.maxHealth = this.health
+        this.diceHtml = getDicePlaceholderHtml(this.diceCount)
+        this.percent = getPercentage(this.health, this.maxHealth)
+    }
 
-function Character(data) {
-    Object.assign(this, data)
-    this.maxHealth = this.health
-
-    this.diceHtml = getDicePlaceholderHtml(this.diceCount)
-
-    this.setDicePlaceholderHtml = () => {
+    setDicePlaceholderHtml() {
         this.diceHtml = getDicePlaceholderHtml(this.diceCount)
     }
 
-    this.setDiceHtml = () => {
+    setDiceHtml() {
         this.currentDiceScore = getDiceRollArray(this.diceCount);
-        data.currentDiceScore = this.currentDiceScore
         this.diceHtml = this.currentDiceScore.map(dice => `<div class="dice">${dice}</div>`).join("")
     }
 
-    this.takeDamage = function(damage) {
+    takeDamage(damage) {
         damage.map(dice => this.health-=dice)
         if (this.health <= 0) {
             this.health = 0
@@ -25,10 +25,7 @@ function Character(data) {
         } 
     }
 
-    this.percent = getPercentage(this.health, this.maxHealth)
-
-
-    this.getHealthBarHtml = () => {
+    getHealthBarHtml() {
         this.oldPercent = this.percent
         this.percent = getPercentage(this.health, this.maxHealth)
         console.log(`name: ${this.charName} || Percent: ${this.percent} || oldPercent: ${this.oldPercent}`);
@@ -40,7 +37,7 @@ function Character(data) {
         </div>`
     }
 
-    this.getCharacterHtml = function() {
+    getCharacterHtml() {
         const { charName, avatar, health, diceCount, currentDiceScore, diceHtml, setDiceHtml } = this;  
         return  `<div class="character-card ${(charName === 'Jedi')?'light_side':''}">
                     <h2 class="name"> ${charName} </h2>
@@ -54,5 +51,63 @@ function Character(data) {
     }
 
 }
+
+
+
+// OLD CONSTRUCTOR FUNCTION
+// 
+// function Character(data) {
+//     Object.assign(this, data)
+//     this.maxHealth = this.health
+
+//     this.diceHtml = getDicePlaceholderHtml(this.diceCount)
+
+//     this.setDicePlaceholderHtml = () => {
+//         this.diceHtml = getDicePlaceholderHtml(this.diceCount)
+//     }
+
+//     this.setDiceHtml = () => {
+//         this.currentDiceScore = getDiceRollArray(this.diceCount);
+//         data.currentDiceScore = this.currentDiceScore
+//         this.diceHtml = this.currentDiceScore.map(dice => `<div class="dice">${dice}</div>`).join("")
+//     }
+
+//     this.takeDamage = function(damage) {
+//         damage.map(dice => this.health-=dice)
+//         if (this.health <= 0) {
+//             this.health = 0
+//             this.isDead = true
+//         } 
+//     }
+
+//     this.percent = getPercentage(this.health, this.maxHealth)
+
+
+//     this.getHealthBarHtml = () => {
+//         this.oldPercent = this.percent
+//         this.percent = getPercentage(this.health, this.maxHealth)
+//         console.log(`name: ${this.charName} || Percent: ${this.percent} || oldPercent: ${this.oldPercent}`);
+//         return `
+//         <div class="health-bar-outer">
+//             <div class="health-bar-inner ${(this.percent<25)? 'danger' : ''}" 
+//                 style="width: ${this.percent}%; --from-width:${this.oldPercent}%; --to-width:${this.percent}%; animation:progress-bar 1s;">
+//             </div>
+//         </div>`
+//     }
+
+//     this.getCharacterHtml = function() {
+//         const { charName, avatar, health, diceCount, currentDiceScore, diceHtml, setDiceHtml } = this;  
+//         return  `<div class="character-card ${(charName === 'Jedi')?'light_side':''}">
+//                     <h2 class="name"> ${charName} </h2>
+//                     <img class="avatar" src="${avatar}" />
+//                     <div class="health">health: <b> ${health} </b></div>
+//                     ${this.getHealthBarHtml()}
+//                     <div class="dice-container">
+//                         ${diceHtml}
+//                     </div>
+//                 </div>`
+//     }
+
+// }
 
 export default Character
